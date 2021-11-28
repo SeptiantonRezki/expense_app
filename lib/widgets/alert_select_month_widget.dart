1220080faf2fa65d91ca1a1107e56db6a1e4f897
Rizky Ramadhan <jmiryas@gmail.com>
2021-11-28 19:45:29 +0700

    Add list of month in alert dialog

--- lib/widgets/alert_select_month_widget.dart
+++ lib/widgets/alert_select_month_widget.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:spending_app/data/data.dart';
-import 'package:spending_app/models/money_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spending_app/bloc/expense/expense_bloc.dart';
+
+import '../data/data.dart';
+import '../models/money_model.dart';
 
 class AlertSelectMonthWidget extends StatelessWidget {
   const AlertSelectMonthWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return StatefulBuilder(builder: (context, setState) {
-      return SingleChildScrollView(
-        child: Column(
-          children: expenseMapList.keys.map((item) {
-            return ListTile(
-              title: Text(getDateTimeString(item)),
-              onTap: () {},
-            );
-          }).toList(),
-        ),
+    return BlocBuilder<ExpenseBloc, ExpenseState>(builder: (context, state) {
+      if (state is ExpenseLoadingState) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+
+      if (state is ExpenseLoadedState) {
+        if (state.expenseMapList.keys.isEmpty) {
+          return const Center(
+            child: Text("Pengeluaran masih kosong."),
+          );
+        } else {
+          return SingleChildScrollView(
+            child: Column(
+              children: state.expenseMapList.keys.map((item) {
+                return ListTile(
+                  title: Text(getDateTimeString(item)),
+                  onTap: () {},
+                );
+              }).toList(),
+            ),
+          );
+        }
+      }
+
+      return const Center(
+        child: Text("Something went wrong!"),
       );
     });
   }
