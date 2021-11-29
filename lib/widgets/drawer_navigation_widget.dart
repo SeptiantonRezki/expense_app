f26036b10cf8c51e08110d3610110bad29181cdc
Rizky Ramadhan <jmiryas@gmail.com>
2021-11-29 13:06:18 +0700

    Add category expense menu

--- lib/widgets/drawer_navigation_widget.dart
+++ lib/widgets/drawer_navigation_widget.dart
@@ -30,18 +30,73 @@ class DrawerNavigationWidget extends StatelessWidget {
             title: Row(
               children: const [
                 Icon(
-                  Icons.home,
+                  Icons.event_note,
+                  color: Colors.black,
                 ),
                 SizedBox(
                   width: 10.0,
                 ),
                 Text(
-                  "Pengeluaran",
+                  "Kategori Pengeluaran",
                 )
               ],
             ),
             onTap: () {
-              print("Pengeluaran");
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text(
+                        "Kategori Pengeluaran",
+                        textAlign: TextAlign.center,
+                      ),
+                      content: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            ListTile(
+                              leading: CircleAvatar(
+                                radius: 10.0,
+                                backgroundColor: Colors.blue.shade400,
+                              ),
+                              title: const Text("Primer"),
+                              subtitle: const Text(
+                                "Kebutuhan primer yaitu kebutuhan pokok yang mutlak dipenuhi oleh semua manusia yaitu pakaian, makanan, dan tempat tinggal. Kebutuhan primer ini merupakan hal yang paling penting untuk dipenuhi guna melanjutkan keberlangsungan hidup.",
+                                textAlign: TextAlign.justify,
+                              ),
+                            ),
+                            ListTile(
+                              leading: CircleAvatar(
+                                radius: 10.0,
+                                backgroundColor: Colors.orange.shade400,
+                              ),
+                              title: const Text("Sekunder"),
+                              subtitle: const Text(
+                                "Kebutuhan sekunder merupakan pelengkap agar dapat menjalankan kehidupan yang lebih baik. Gaya hidup manusia merupakan salah satu hal kebutuhan sekunder.",
+                                textAlign: TextAlign.justify,
+                              ),
+                            ),
+                            ListTile(
+                              leading: CircleAvatar(
+                                radius: 10.0,
+                                backgroundColor: Colors.red.shade400,
+                              ),
+                              title: const Text("Tersier"),
+                              subtitle: const Text(
+                                "Kebutuhan tersier merupakan kebutuhan yang sering sebagian orang disebut kebutuhan akan sesuatu yang bersifat mewah. Tujuan dari pemenuhan kebutuhan tersier ialah untuk kesenangan pribadi.",
+                                textAlign: TextAlign.justify,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.pop(context, "OK"),
+                          child: const Text("OK"),
+                        ),
+                      ],
+                    );
+                  });
             },
           ),
           ListTile(
@@ -54,7 +109,7 @@ class DrawerNavigationWidget extends StatelessWidget {
                   width: 10.0,
                 ),
                 Text(
-                  "Pilih Bulan",
+                  "Pilih Bulan Pengeluaran",
                 )
               ],
             ),
